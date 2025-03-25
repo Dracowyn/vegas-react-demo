3e4d16361d33ee437a4f9c487441aa79a25864ae
Dracowyn <dracowyn@outlook.com>
2025-03-25 11:48:56 +0800

    dts配置

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as Vegas } from './components/Vegas';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react(), dts(
+		{
+			exclude: ['src/pages/**', 'src/assets/**', 'src/images/**', 'public/**'],
+		}
+	)],
+	build: {
+		lib: {
+			entry: 'src/index.ts',
+			name: 'ReactVegas',
+			fileName: (format) => `react-vegas.${format}.js`,
+			formats: ['es', 'cjs'],
+		},
+		rollupOptions: {
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
 })
